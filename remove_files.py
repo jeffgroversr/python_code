e0d67726d90862a61c39df3730d6ec8f84f15810
Dr Jeff Grover <drjeffgrover@gmail.com>
2019-11-28 14:47:58 -0500

    Add files via upload
    How to remove files using the os library.

---
+++ remove_files.py
@@ -0,0 +1,6 @@
+﻿import os
+os.remove('data/dict_louisville_Right_of_Way_Permits.csv')
+os.remove('data/louisville_Right_of_Way_Permits.xlsx')
+os.remove('data/read_louisville_Right_of_Way_Permits.csv')
+
+
